de63b357671ea69a55da3d41b49076ae9cb3b2f3
hao2432 <hao2432@gmail.com>
2016-04-06 13:21:52 +0800

    修改

--- ueditor.py
+++ ueditor.py
@@ -1,7 +1,8 @@
 # coding:utf-8
 __author__ = 'chenghao'
-from bottle import Bottle, request
-import os, re
+from bottle import Bottle, request, static_file
+import os, re, json
+from datetime import datetime
 
 ueditor_bottle = Bottle()
 config_json = {}
@@ -18,7 +19,14 @@ def ueditor_get():
 def ueditor_post():
 	action = request.query.get("action")
 	files = request.files.getall("upfile")
-	dispatch(action, files)
+	res = dispatch(action, files)
+	return res
+
+
+@ueditor_bottle.get('/upload/<filename:path>')
+def static(filename):
+	""" Serve static files """
+	return static_file(filename, root='./upload/')
 
 
 def get_json_data(action):
@@ -36,11 +44,103 @@ def get_json_data(action):
 	return config_json
 
 
+def get_full_name(path_format):
+	now = datetime.now()
+	d = now.strftime("%Y-%y-%m-%d-%H-%M-%S").split("-")
+	path_format = path_format.replace("{yyyy}", d[0])
+	path_format = path_format.replace("{yy}", d[1])
+	path_format = path_format.replace("{mm}", d[2])
+	path_format = path_format.replace("{dd}", d[3])
+	path_format = path_format.replace("{hh}", d[4])
+	path_format = path_format.replace("{ii}", d[5])
+	path_format = path_format.replace("{ss}", d[6])
+	return path_format
+
+
+def create_directory(path):
+	if not os.path.exists(path):
+		os.makedirs(path)
+
+
+def get_path_format(action, obj):
+	if action == "uploadimage":
+		return obj.get("imagePathFormat")
+	elif action == "uploadscrawl":
+		return obj.get("scrawlPathFormat")
+	elif action == "catchimage":
+		return obj.get("catcherPathFormat")
+	elif action == "uploadvideo":
+		return obj.get("videoPathFormat")
+	elif action == "uploadfile":
+		return obj.get("filePathFormat")
+	elif action == "uploadsnapscreen":
+		return obj.get("snapscreenPathFormat")
+	else:
+		return "/ueditor/py/upload/custom"
+
+
+def get_file_path(path_s):
+	return os.getcwd() + path_s
+
+
 def dispatch(action, files):
 	"""
 	任务分发
-	:param action: uploadimage上传图片, uploadscrawl上传涂鸦图片, catchimage上传远程图片, uploadvideo上传视频,
-					uploadfile上传文件
+	:param action: uploadimage 上传图片, uploadscrawl 上传涂鸦图片, catchimage 上传远程图片, uploadvideo 上传视频,
+					uploadfile 上传文件, uploadsnapscreen 上传屏幕截图
 	:return:
 	"""
-	print config_json
+	obj = json.loads(config_json)
+	absolute_path = obj["absolutePath"]  # 绝对路经
+	if absolute_path:  # 使用绝对路经保存
+		create_directory(absolute_path)
+		file_path = absolute_path
+	else:  # 使用相对路经保存
+		path_format = get_full_name(get_path_format(action, obj))
+		path = get_file_path(path_format)
+		create_directory(path)
+		file_path = path
+
+	if action == "uploadimage":
+		res = upload_image(files, obj["imageFieldName"], obj["imageMaxSize"], obj["imageAllowFiles"], file_path,
+						   path_format)
+	elif action == "uploadscrawl":
+		pass
+	elif action == "catchimage":
+		pass
+	elif action == "uploadvideo":
+		pass
+	elif action == "uploadfile":
+		pass
+	elif action == "uploadsnapscreen":
+		pass
+	elif action == "listimage":
+		pass
+	elif action == "listfile":
+		pass
+	else:
+		pass
+
+	return res
+
+
+def upload_image(files, max_size, allow_files, file_path, path_format):
+	res = {}
+	for f in files:
+		file_name = f.filename
+		file_size = os.path.getsize(f.file.name)
+
+		name, ext = os.path.splitext(file_name)
+		if ext not in allow_files:  # 上传的图片格式不正确
+			res['state'] = "上传的图片格式不正确"
+
+		if long(max_size) < file_size:  # 图片太大
+			res['state'] = "图片太大"
+
+		if not res:  # 判断该dict是否为空
+			f.save(file_path, overwrite=True)
+			res["state"] = "SUCCESS"
+			res["url"] = path_format + "/" + file_name
+
+		return res
+
